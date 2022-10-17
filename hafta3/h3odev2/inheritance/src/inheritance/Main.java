7c7f002948fd1e4fbcb7edcc52236bb194224b38
yunusemrecakmak <114932916+yunusemrecakmak@users.noreply.github.com>
2022-10-18 02:05:04 +0300

    Add files via upload

---
+++ hafta3/h3odev2/inheritance/src/inheritance/Main.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+		customer.age= 15;
+		employee.salary= 5600.13;
+		
+
+	}
+
+}
